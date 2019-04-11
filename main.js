d91c39fce1ec8b56a5690978b4ce91aeb5c05d43
rush <rushastronomo0415@gmail.com>
2019-04-11 09:24:25 -0700

    adds initial project files

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-// write your code here
-
-});
+    // write your code here
+    $.getJSON( "data.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<tr><td>" + val.name + "</td><td>" + val.description + "</td><td><a href='https://www.google.com/maps?q=" + val.location + "'>link</a></td></tr>" );
+        });
+    
+        $( "<table/>", {
+            "name": "",
+            "description": "",
+            "location": "",
+            html: items.join( "" )
+          }).appendTo( "body" );
+        });
+      });
